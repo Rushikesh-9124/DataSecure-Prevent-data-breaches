0aaf83aba00017f8e06c70673e4937be46ee1a60
Rushikesh-9124 <rushikesh6281@gmail.com>
2025-11-30 20:56:47 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:5001',
+    baseURL:'https://data-secure-prevent-data-breaches.vercel.app',
     timeout: 60000,
     headers: {
         "Content-Type": "application/json"
